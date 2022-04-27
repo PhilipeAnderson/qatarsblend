8e9587afb5b70607b0f396f1c0ef7ec5139501cd
philipe_kraken <philipeacampos@hotmail.com>
2022-04-27 00:25:38 -0300

    Fix the problem and close the Issue in github issue: metro.config.js #1

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
-import { theme } from './src/styles/theme';
+import theme from './src/styles/theme';
 
 // import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 // import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
