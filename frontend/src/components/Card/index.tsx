34e97bf15f89732e82f80e7aa005a489f15d09ea
philipe_kraken <philipeacampos@hotmail.com>
2022-04-29 20:05:51 -0300

    Docked the elements inside the container

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+  Container, 
+  ContentInfo, 
+  Brand, 
+  Name, 
+  Daily, 
+  Value,
+  ContentCar
+
+} from './styles';
 
 export const Card = () => {
   return (
     <Container>
-      <Title> Hy Card </Title>
+      <ContentInfo>
+        <Brand> Audi </Brand>
+        <Name> RS 5 Coupe </Name>
+        <Daily> Per Day </Daily>
+        <Value> U$: 120.00</Value>
+      </ContentInfo>
+      <ContentCar />
     </Container>
   );
 };
--- frontend/src/components/Card/styles.ts
+++ frontend/src/components/Card/styles.ts
@@ -8,10 +8,41 @@ export const Container = styled.View`
   border-radius: 10px;
   margin: 14px auto;
   background-color: ${({ theme }) => theme.colors.backgroundPrimary};
+
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
 `;
 
-export const Title = styled.Text`
+export const ContentInfo = styled.View`
+  width: 150px;
+  height: 150px;
+  background-color: red;
+`;
+
+export const Brand = styled.Text`
   color: #FFFFFF;
   font-size: 26px;
+`;
+
+export const Name = styled.Text`
+  color: #FFFFFF;
+  font-size: 26px;
+`;
+
+export const Daily = styled.Text`
+  color: #FFFFFF;
+  font-size: 26px;
+`;
+
+export const Value = styled.Text`
+  color: #FFFFFF;
+  font-size: 26px;
+`;
+
+export const ContentCar = styled.View`
+  width: 200px;
+  height: 150px;
+  background-color: purple;
 `;
 
